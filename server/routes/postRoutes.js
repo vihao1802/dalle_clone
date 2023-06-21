a49801872ace24a2b098ee8f91609099d7a3a3f5
2NgonNgon2 <tranvihao40@gmail.com>
2023-06-21 21:37:09 +0700

    fix deploy backend to render 2.0

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -13,7 +13,8 @@ const router = express.Router();
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
 })
 
 // GET ALL POSTS
